63f95b34339a608502c7289f48c36a1c27e143de
Carlos Mosquera <camosqueraba@gmail.com>
2021-02-02 23:57:24 -0500

    declarando variable, operadores y cadenas

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log('hola mundo');
+/*console.log('hola mundo');
+var a = 1;
+let b = 2;
+const c = 3;
+
+if (true) {
+    a = 11;
+    let b;
+    b = b*2;
+    const c = 4;
+}
+console.log(a);
+console.log(b);
+console.log(c);
+
+let string = 1;
+let string2 = "2";
+
+suma = string + string2;
+
+console.log(suma);*/
+
+let string = `Down on "\\n" the sea`
+let string2 = "Lie on the ocean"
+let string3 = 'Float on the ocean'
+console.log(string);
+console.log(string2);
+console.log(string3);
+console.log("A newline character is written like \"\\n\".");
+console.log(- (10 - 2))
+console.log(3 > 2)
+// → true
+console.log(3 < 2)
+// → false
+console.log("bardvark" < "aoroer")
+// → true
+console.log("Itchy" != "Scratchy")
+// → true
+console.log("Apple" == "Orange")
+// → false
+console.log(NaN == NaN)
+// → false
+console.log(true && false)
+// → false
+console.log(true && true)
+// → true
+console.log(false || true)
+// → true
+console.log(false || false)
+// → false
+console.log(true ? 1 : 2);
+// → 1
+console.log(false ? 1 : 2);
+// → 2
